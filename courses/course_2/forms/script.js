a37574f46ef0c6b60afd623a1e1702a10d580bd8
moriVotum <nacbboom@gmail.com>
2023-03-16 14:19:48 +0300

    forms changes

--- courses/course_2/forms/script.js
+++ courses/course_2/forms/script.js
@@ -7,6 +7,9 @@ const pswd = document.querySelector(".pswd");
 const errorMessage = document.querySelector(".error-message");
 const subBut = document.querySelector(".subBut");
 const formSend = document.querySelector(".form-send");
+const mail = document.querySelector(".mail");
+
+let s = "";
 
 function closeModal() {
   const overlay = document.querySelector(".overlay");
@@ -53,27 +56,49 @@ formSend.addEventListener("submit", (e) => {
 });
 
 showPass.addEventListener("pointerdown", (e) => {
-  e.stopPropagation();
   pswd.type = "text";
 });
 
 showPass.addEventListener("pointerup", (e) => {
-  e.stopPropagation();
   pswd.type = "password";
 });
 
-pswd.addEventListener("blur", (e) => {
-  e.stopPropagation();
+function errors()
+{
+  s = "";
+  if (mail.value === "") {
+    errorMessage.classList.add("hidden-er");
+    s += "Почта обязательна для заполнения\n";
+    mail.setCustomValidity("Почта обязательна для заполнения\n");
+  }
+  if (mail.validity.typeMismatch) {
+    errorMessage.classList.add("hidden-er");
+    s += "Почта заполнена неправильно\n";
+    mail.setCustomValidity("Почта заполнена неправильно\n");
+  }
   if (pswd.value === "") {
     errorMessage.classList.add("hidden-er");
-    errorMessage.textContent = "Поле обязательно для заполнения";
-    pswd.setCustomValidity("Поле обязательно для заполнения");
+    s += "Пароль обязателен для заполнения ";
+    pswd.setCustomValidity("Пароль обязателен для заполнения ");
   } else if (pswd.value.length < 6) {
     errorMessage.classList.add("hidden-er");
-    errorMessage.textContent = "Пароль должен быть не менее 6 символов";
+    s += "Пароль должен быть не менее 6 символов ";
     pswd.setCustomValidity("Пароль должен быть не менее 6 символов");
+  }
+
+  if (mail.value === "" || pswd.value === "" || pswd.value.length < 6 || mail.validity.typeMismatch) {
+    errorMessage.textContent = s;
   } else {
     errorMessage.classList.remove("hidden-er");
     pswd.setCustomValidity("");
+    mail.setCustomValidity("");
   }
+}
+
+pswd.addEventListener("blur", (e) => {
+  errors();
+});
+
+mail.addEventListener("blur", (e) => {
+  errors();
 });
---
+++ courses/course_2/node/app.js
@@ -0,0 +1,4 @@
+const http = require("node:http");
+
+const host = '127.0.0.1';
+const port = 5500;
