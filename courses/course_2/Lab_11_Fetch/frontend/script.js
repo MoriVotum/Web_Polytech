df26cf26643d70e96f65ce26078be1804e93c0a1
moriVotum <nacbboom@gmail.com>
2023-05-24 00:55:21 +0300

    frontend ez

---
+++ courses/course_2/Lab_11_Fetch/frontend/script.js
@@ -0,0 +1,174 @@
+const loginName = document.querySelector("#loginName");
+const getApiKey = document.querySelector("#getApiKey");
+const refresh = document.querySelector("#refresh");
+const table = document.querySelector(".models");
+
+console.log("loginName:", loginName);
+console.log("getApiKey:", getApiKey);
+
+let apiKey = "";
+
+getApiKey.addEventListener("click", () => {
+  console.log("getApiKey");
+  const name = loginName.value;
+  console.log("name:", name);
+  fetch(`/v3/user/getUser?name=${name}`)
+    .then((response) => {
+      console.log("response:", response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log("data:", data);
+      if (data == null) {
+        alert("Нет такого пользователя");
+        apiKey = "";
+        loginName.style.backgroundColor = "#FFFFFF";
+        return;
+      }
+      loginName.style.backgroundColor = "#AAC789";
+      apiKey = data.api_key;
+      console.log("apiKey:", apiKey);
+    })
+    .catch((err) => {
+      console.log("err:", err);
+      alert("Произошла ошибка при получении api_key");
+    });
+});
+
+loginName.addEventListener("keydown", (e) => {
+  console.log("e.key:", e.key);
+  if (e.key == "Enter") {
+    console.log("Enter");
+    getApiKey.click();
+  }
+});
+
+function getAllModels() {
+  fetch(`/v3/user/models`)
+    .then((response) => {
+      console.log("response:", response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log("data:", data);
+      if (data == null || data.length == 0) {
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        td1.innerHTML = "No models";
+        tr.appendChild(td1);
+        table.appendChild(tr);
+        return;
+      }
+      console.log("table:", table);
+      let i = 0;
+      data.forEach((element) => {
+        i++;
+        console.log("element:", element);
+        const tr = document.createElement("tr");
+        const td1 = document.createElement("td");
+        // buttons for show model and delete model here
+        const buttonShow = document.createElement("button");
+        buttonShow.innerHTML = "Show";
+        buttonShow.classList.add("btn_show");
+        buttonShow.dataset.id = element._id;
+        const buttonDelete = document.createElement("button");
+        buttonDelete.innerHTML = "Delete";
+        buttonDelete.classList.add("btn_del");
+        buttonDelete.dataset.id = element._id;
+        td1.innerHTML = "name model " + i + ": " + element.name_model;
+        const divBut = document.createElement("div");
+        divBut.appendChild(buttonShow);
+        divBut.appendChild(buttonDelete);
+        tr.appendChild(td1);
+        tr.appendChild(divBut);
+        table.appendChild(tr);
+      });
+    })
+    .catch((err) => {
+      console.log("err:", err);
+      alert("Произошла ошибка при получении моделей");
+    });
+}
+
+table.addEventListener("click", (e) => {
+  console.log("e.target:", e.target);
+  console.log("e.target.classList:", e.target.classList);
+  console.log("e.target.dataset.id:", e.target.dataset.id);
+  if (e.target.classList.contains("btn_show")) {
+    console.log("show model");
+    const id = e.target.dataset.id;
+    console.log("id:", id);
+    fetch(`/v3/user/models/${id}`)
+      .then((response) => {
+        console.log("response:", response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data:", data);
+        if (data == null) {
+          alert("Нет такой модели лель");
+          return;
+        }
+        // show model
+        alert(
+          "Model: " +
+            data.name_model +
+            "\n" +
+            "Description: " +
+            data.description +
+            "\n" +
+            "Comments: " +
+            data.comments
+        );
+      })
+      .catch((err) => {
+        console.log("err:", err);
+        alert("Произошла ошибка при получении модели");
+      });
+  }
+  if (e.target.classList.contains("btn_del")) {
+    console.log("delete model");
+    const id = e.target.dataset.id;
+    console.log("id:", id);
+    console.log("apiKey:", apiKey);
+    fetch(`/v3/user/models/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        api_key: apiKey,
+      },
+    })
+      .then((response) => {
+        console.log("response:", response);
+        if (response.statusText == "Unauthorized") {
+          alert("Не авторизован");
+          return;
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data:", data);
+        if (data == null) {
+          //   alert("No such model");
+          return;
+        }
+        // delete model
+        alert("Model deleted");
+        table.innerHTML = "";
+
+        getAllModels();
+      })
+      .catch((err) => {
+        console.log("err:", err);
+        alert("Произошла ошибка при удалении модели");
+      });
+  }
+});
+
+refresh.addEventListener("click", () => {
+  console.log("refresh");
+  table.innerHTML = "";
+  getAllModels();
+});
+
+getAllModels();
---
+++ courses/course_2/Lab_11_Fetch/services/module.js
@@ -0,0 +1,110 @@
+import { comments } from "./dataServer.js";
+import { requests } from "./dataServer.js";
+import { user } from "./dataServer.js";
+
+const rootHandler = (req, res) => {
+  res.send("Hello world!\n");
+};
+
+const requestsHandler = (req, res, next) => {
+  console.log("ZAPROS");
+  const name = req.headers["user-agent"];
+  requests[name]
+    ? requests[name]++
+    : ((requests[name] = 1), console.log("Новое подключение!"));
+  next();
+};
+
+const commentsHandler = (req, res) => {
+  let data = "";
+  data = req.body();
+  req.on("end", () => {
+    if (data) comments.push(data);
+    res.json(comments);
+  });
+};
+
+const statsHandler = (req, res) => {
+  let html = "<table>\n";
+  for (const key in requests) {
+    html += "<tr>\n";
+    html += `<td>${key}</td>\n`;
+    html += `<td>${requests[key]}</td>\n`;
+    html += "</tr>\n";
+  }
+  html += "</table>\n";
+  res.send(html);
+};
+
+const allHandler = (req, res) => {
+  if (res.statusMessage == undefined) res.sendStatus(400, "400 Bad Request");
+};
+
+const userHandler = (req, res, next) => {
+  let login = "";
+  let password = "";
+
+  login = req.query.login;
+  password = req.query.password;
+
+  console.log(login, password);
+
+  if (login == user.login && password == user.password) {
+    console.log("admin");
+  } else {
+    res.send("ne Правильные данные!\n");
+  }
+
+  next();
+};
+
+const apiHandler = (req, res, next) => {
+  let apiKey = "";
+
+  apiKey = req.query.apiKey;
+
+  console.log(apiKey);
+
+  if (apiKey == user.apiKey) {
+    res.send("Hello admin!\n");
+    // add message to res body
+  } else {
+    res.send("Api Key is not normal!\n");
+  }
+  next();
+};
+
+const validateHandler = (req, res, next) => {
+  console.log("validateHandler");
+
+  let data = "";
+  // req.on("data", (chunk) => {
+  //   data += chunk;
+  // });
+  data = req.body;
+
+  // req.on("end", () => {
+  if (data) {
+    // console.log("ValidateData:", data);
+    if (data.name && data.text) {
+      next();
+    } else {
+      res.sendStatus(400, "400 Bad Request");
+    }
+  } else {
+    res.sendStatus(400, "400 Bad Request");
+  }
+  // });
+  // next();
+};
+
+export {
+  rootHandler,
+  commentsHandler,
+  statsHandler,
+  requestsHandler,
+  allHandler,
+  userHandler,
+  apiHandler,
+  validateHandler,
+};
